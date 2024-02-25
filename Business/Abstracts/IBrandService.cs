e2f8d2a1c8f043272e092d210d8875b10b33d5f4
Halit Bedii Taner <bediitaner@outlook.com>
2024-02-25 21:30:22 +0300

    Add project files.

---
+++ Business/Abstracts/IBrandService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concretes;
+
+namespace Business.Abstracts
+{
+    public interface IBrandService
+    {
+        CreatedBrandResponse Add(CreateBrandRequest brand);
+        List<Brand> GetAll();
+    } 
+}
